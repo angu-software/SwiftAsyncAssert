9eda904c1b758e6d92de90293752510d765a1247
Andreas Günther <andreas.guenther@runtastic.com>
2023-08-11 19:42:06 +0200

    MOD set min platform versions and swift tools version which support Swift concurrency

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftAsyncAssert",
+    platforms: [.macOS(.v10_15),
+                .iOS(.v13),
+                .watchOS(.v6),
+                .tvOS(.v13)],
     products: [
         .library(
             name: "SwiftAsyncAssert",
